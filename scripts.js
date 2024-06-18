0f21c7220fcfa8c992b6ff4855d9bdd852a5b850
abin0001 <abinj2003@gamail.com>
2024-06-18 16:11:26 +0530

    Completed Quiz app

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ const questions = [
 
 
 const questionElement =document.getElementById("question");
-const answerButton =document.getElementById("answer-buttons");
+const answerButtons =document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 
@@ -58,18 +58,78 @@ function startQuiz(){
 }
 
 function showQuestion(){
-
+    restState();
     let currentQuestion=questions[currentQuestionIndex];
     let questionNo=currentQuestionIndex + 1;
     questionElement.innerHTML=questionNo + "." +currentQuestion.questions;
 
 
-    currentQuestion.answers.froEach(answer =>{
-        const button =document.createElement("button");
+    currentQuestion.answers.forEach(answer =>{
+        const button = document.createElement("button");
         button.innerHTML=answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct=answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
     });
 }
+
+function restState(){
+    nextButton.style.display="none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn=e.target;
+    const isCorrect =selectedBtn.dataset.correct === "true";
+
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct ==="true"){
+            button.classList.add("correct");
+
+        }
+        button.disabled="true";
+    });
+    nextButton.style.display="block";
+}
+
+function showScore(){
+    restState()
+    questionElement.innerHTML =`You Scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML ="Play Again";
+    nextButton.style.display="block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+});
 
 startQuiz();
