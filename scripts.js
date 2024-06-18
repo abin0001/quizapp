dcc47d210e357115444f3c9a62c32b7c595d8388
abin0001 <abinj2003@gamail.com>
2024-06-18 13:39:59 +0530

    Js initiazied

---
+++ scripts.js
@@ -0,0 +1,56 @@
+const questions = [
+    {
+        questions:"what is largest animal in the world ",
+        answers:[
+                {text:"shark", correct:false},
+                {text:"Blue Whale", correct: true},
+                {text:"Elephat", correct:false},
+                {text:"Giraffe", correct:false},
+        ]
+    },    {
+        questions:"what is largest animal in the world ",
+        answers:[
+                {text:"shark", correct:false},
+                {text:"Blue Whale", correct: true},
+                {text:"Elephat", correct:false},
+                {text:"Giraffe", correct:false},
+        ]
+    },
+    
+    {
+        questions:"what is tallest animal in the world ",
+        answers:[
+                {text:"shark", correct:false},
+                {text:"Blue Whale", correct: false},
+                {text:"Elephat", correct:false},
+                {text:"Giraffe", correct:true},
+        ]
+    },
+
+    
+    {
+        questions:"what is havyest animal in the world ",
+        answers:[
+                {text:"shark", correct:false},
+                {text:"Blue Whale", correct:false},
+                {text:"Elephat", correct:true},
+                {text:"Giraffe", correct:false},
+        ]
+    },
+    
+
+    {
+        questions:"what is biggest animal in the world ",
+        answers:[
+                {text:"shark", correct:false},
+                {text:"Blue Whale", correct: true},
+                {text:"Elephat", correct:false},
+                {text:"Giraffe", correct:false},
+        ]
+    },
+]
+
+
+const questionElement =document.getElementsById("question");
+const answerButton =document.getElementsById("answer-buttons");
+const nextButton = documnet.getElementsById("next-btn");
