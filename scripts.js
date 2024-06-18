d3c54f777b7e3b114768af26e433c2d42e75190f
abin0001 <abinj2003@gamail.com>
2024-06-18 14:36:46 +0530

    custom questions were added

--- scripts.js
+++ scripts.js
@@ -7,16 +7,8 @@ const questions = [
                 {text:"Elephat", correct:false},
                 {text:"Giraffe", correct:false},
         ]
-    },    {
-        questions:"what is largest animal in the world ",
-        answers:[
-                {text:"shark", correct:false},
-                {text:"Blue Whale", correct: true},
-                {text:"Elephat", correct:false},
-                {text:"Giraffe", correct:false},
-        ]
     },
-    
+
     {
         questions:"what is tallest animal in the world ",
         answers:[
@@ -27,7 +19,6 @@ const questions = [
         ]
     },
 
-    
     {
         questions:"what is havyest animal in the world ",
         answers:[
@@ -37,7 +28,6 @@ const questions = [
                 {text:"Giraffe", correct:false},
         ]
     },
-    
 
     {
         questions:"what is biggest animal in the world ",
@@ -51,6 +41,35 @@ const questions = [
 ]
 
 
-const questionElement =document.getElementsById("question");
-const answerButton =document.getElementsById("answer-buttons");
-const nextButton = documnet.getElementsById("next-btn");
+const questionElement =document.getElementById("question");
+const answerButton =document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+
+let currentQuestionIndex=0;
+let score=0;
+
+
+function startQuiz(){
+    currentQuestionIndex=0;
+    score =0;
+    nextButton.innerHTML ="Next"
+    showQuestion();
+}
+
+function showQuestion(){
+
+    let currentQuestion=questions[currentQuestionIndex];
+    let questionNo=currentQuestionIndex + 1;
+    questionElement.innerHTML=questionNo + "." +currentQuestion.questions;
+
+
+    currentQuestion.answers.froEach(answer =>{
+        const button =document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
+startQuiz();
